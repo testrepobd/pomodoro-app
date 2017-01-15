c1b9138ab3bce3d6734154b9d1501b87a6333232
testrepobd <testrepobd@mailinator.com>
2017-01-15 15:03:35 -0500

    Restructured this dir. Had all project dirs in this folder, but each project now has its own repo

--- Todo App/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TodoApp from './App';
-import './index.css';
-
-ReactDOM.render(
-  <TodoApp/>,
-  document.getElementById('root')
-);
--- Weather App/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import WeatherApp from './App';
-import './index.css';
-
-ReactDOM.render(
-  <WeatherApp />,
-  document.getElementById('root')
-);
